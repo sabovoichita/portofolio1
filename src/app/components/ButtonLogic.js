a08a41d015d88be5d7f93f9cdc6b539f3cbe971b
Voichita Maria <sabo_voichita@yahoo.com>
2025-03-11 18:55:52 +0100

    add ButtonLogic Component

---
+++ src/app/components/ButtonLogic.js
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+
+const ButtonLogin = ({ isLoggedIn, name, extraStyle }) => {
+  if (isLoggedIn) {
+    return (
+      <Link
+        href="/dashboard"
+        className={`btn btn-primary ${extraStyle ? extraStyle : ""}`}
+      >
+        Welcome back {name}
+      </Link>
+    );
+  }
+
+  return <button>Login</button>;
+};
+
+export default ButtonLogin;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import ButtonLogin from "./components/ButtonLogic";
 
 export default function Home() {
+  const isLoggedIn = true;
+  const name = "Maria";
   return (
     <div
       data-theme="dark"
       className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <button className="btn btn-accent">Click</button>
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
         <Image
           className="dark:invert"
           src="/next.svg"
